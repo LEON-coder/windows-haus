3427f9e20c95e082a998017ac86da59668a07ab6
Leonid <leonid.spb2015@yandex.ru>
2023-08-22 21:24:58 +0300

    Добавил модальные окна для блоков- 'установка пластиковых окон' и ' остекление лоджий'

--- build/js/pop_up.js
+++ build/js/pop_up.js
@@ -2,7 +2,7 @@ let frameModal = document.querySelector('.call-measurer-home__background');
 let pushButtonMeasurement = document.querySelector('.advantage-block-link');
 let closeModalMeasurement = document.querySelector('.call-measurer-home__cross')
 let requestMeasurementHeader = document.querySelector('.request-for-measurement')
-let orderPaymentCalcModal = document.querySelector('.order_payment__background');
+let orderCalcModal = document.querySelector('.order_payment__background');
 let offerCalcLink = document.querySelector('.offer-calc-block');
 let orderPaymentClose = document.querySelector('.order_payment__close');
 let orderPaymentCloseHeader = document.querySelector('.order-payment__close-header');
@@ -10,7 +10,7 @@ let orderPaymentHeaderLink = document.querySelector('.order-payment-block-header
 let orderPaymentModalHeader = document.querySelector('.order-payment__background-header');
 
 
-// Функция открытия и закрытия модального окна для блока about_company
+// Функция открытия и закрытия модального окна для ссылки заявка на замер в блоке about_company
 pushButtonMeasurement.addEventListener('click',function () {
 	frameModal.classList.toggle('call-measurer-home__background-open');
 });
@@ -29,6 +29,36 @@ requestMeasurementHeader.addEventListener('click',function () {
 
 
 
+// Функция открытия модального окна для ссылки Установка пластиковых окон в блоке our_services__wrap
+
+let blockServiceLink = document.querySelector('.open-first-modal');
+let popupOurServicesBackground = document.querySelector('.pop-up__our-services__background');
+let popupOurServicesClose = document.querySelector('.pop-up__our-services_close')
+
+blockServiceLink.addEventListener('click',function () {
+	popupOurServicesBackground.classList.toggle('pop-up__our-services__background-open');
+});
+
+popupOurServicesClose.addEventListener('click',function () {
+	popupOurServicesBackground.classList.remove('pop-up__our-services__background-open');
+});
+
+// Функция открытия модального окна для ссылки Остекление балконов и лоджий в блоке our_services__wrap
+let blockServiceLink2 = document.querySelector('.open-second-modal');
+let popupOurServicesBackground2 = document.querySelector('.pop-up-2__our-services__background');
+let popupOurServicesClose2 = document.querySelector('.pop-up__our-services_close-2')
+
+
+blockServiceLink2.addEventListener('click',function () {
+	popupOurServicesBackground2.classList.toggle('pop-up-2__our-services__background-open');
+});
+
+popupOurServicesClose2.addEventListener('click',function () {
+	popupOurServicesBackground2.classList.remove('pop-up-2__our-services__background-open');
+});
+
+
+
 
 // Функция открытия модального окна для ссылки заказать расчет в блоке header 
 orderPaymentHeaderLink.addEventListener('click',function () {
@@ -42,11 +72,11 @@ orderPaymentCloseHeader.addEventListener('click',function () {
 
 // Функция открытия модального окна для ссылки заказать расчет в блоке quality-items-block
 offerCalcLink.addEventListener('click',function () {
-	orderPaymentCalcModal.classList.toggle('order_payment__background-open');
+	orderCalcModal.classList.toggle('order_payment__background-open');
 });
 
 orderPaymentClose.addEventListener('click',function () {
-	orderPaymentCalcModal.classList.remove('order_payment__background-open');
+	orderCalcModal.classList.remove('order_payment__background-open');
 });
 
 
