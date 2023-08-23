11ccc5719845e1ea7a7e990ccdc4d6844759930c
Leonid <leonid.spb2015@yandex.ru>
2023-08-23 19:09:41 +0300

    Верстка pop-up для блока отзывов

--- build/js/pop_up.js
+++ build/js/pop_up.js
@@ -58,6 +58,35 @@ popupOurServicesClose2.addEventListener('click',function () {
 });
 
 
+// Функция открытия модального окна для ссылки Остекление котеджей и дач в блоке our_services__wrap
+
+let blockServiceLink3 = document.querySelector('.open-third-modal');
+let popupOurServicesBackground3 = document.querySelector('.pop-up-3__our-services__background');
+let popupOurServicesClose3 = document.querySelector('.pop-up__our-services_close-3');
+
+blockServiceLink3.addEventListener('click',function () {
+	popupOurServicesBackground3.classList.toggle('pop-up-3__our-services__background-open');
+});
+
+popupOurServicesClose3.addEventListener('click',function () {
+	popupOurServicesBackground3.classList.remove('pop-up-3__our-services__background-open');
+});
+
+// Функция открытия модального окна для ссылки Остекление корпоративных обьектов our_services__wrap
+
+let blockServiceLink4 = document.querySelector('.open-fourth-modal');
+let popupOurServicesBackground4 = document.querySelector('.pop-up-4__our-services__background');
+let popupOurServicesClose4 = document.querySelector('.pop-up__our-services_close-4');
+
+blockServiceLink4.addEventListener('click',function () {
+	popupOurServicesBackground4.classList.toggle('pop-up-4__our-services__background-open');
+});
+
+popupOurServicesClose4.addEventListener('click',function () {
+	popupOurServicesBackground4.classList.remove('pop-up-4__our-services__background-open');
+});
+
+
 
 
 // Функция открытия модального окна для ссылки заказать расчет в блоке header 
@@ -77,6 +106,22 @@ offerCalcLink.addEventListener('click',function () {
 
 orderPaymentClose.addEventListener('click',function () {
 	orderCalcModal.classList.remove('order_payment__background-open');
+});
+
+
+// Функция открытия модального окна для ссылки "оставить отзыв" во вкладке about-company__reviews-block
+
+let linkOpenRewiev = document.querySelector('.about-company__leave-review-block')
+let rewievBlogBackground = document.querySelector('.rewiev-blog__background')
+let rewievBlogClose = document.querySelector('.rewiev-blog__close')
+
+
+linkOpenRewiev.addEventListener('click',function () {
+	rewievBlogBackground.classList.toggle('rewiev-blog-open');
+});
+
+rewievBlogClose.addEventListener('click',function () {
+	rewievBlogBackground.classList.remove('rewiev-blog-open');
 });
 
 
